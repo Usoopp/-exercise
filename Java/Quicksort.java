60d48ff7026ae0b9607765afcd37c013390249af
monkey <35953989+Usoopp@users.noreply.github.com>
2019-09-19 22:14:01 +0800

    Update Quicksort.java

--- Java/Quicksort.java
+++ Java/Quicksort.java
@@ -1,15 +1,19 @@
 package com.sort_algs;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
 
 import edu.princeton.cs.algs4.StdRandom;
 //快速排序
 public class Quicksort {
 	public static void sort(int[] a) {
-		StdRandom.shuffle(a); 	//打乱
+	StdRandom.shuffle(a); 	//打乱
 		sort(a,0,a.length -1);
 	}
 	
 	private static void sort(int[] a,int lo, int hi) {
-		if (hi <= lo) return;
+		if (hi <= lo) return;          //只有一个元素时
 		int j = partition1(a,lo,hi);   //切分
 		sort(a, lo, j-1);				  //递归调用将左半部分a[lo,...,j-1]   排序 
 		sort(a, j+1, hi);   			  //递归调用将右半部分a[j+1,...,hi] 排序
@@ -17,7 +21,7 @@ public class Quicksort {
 	
 	//pivot随着每次交换
 	public static int partition(int[] a,int lo, int hi) {
-		int i = lo, j = hi + 1;
+		int i = lo, j = hi;
 		int v = a[lo];    //pivot, 切分元素
 		while(i < j) {
 			while (i < j && a[j] > v) {
@@ -40,33 +44,54 @@ public class Quicksort {
 	}
 	
 	//pivot先固定不动，最后交换
-	public static int partition1(int[] a,int lo, int hi) {  
-		int i = lo, j = hi;
-		int v = a[lo];    //pivot, 切分元素
-		while(i < j) {
-			while (i < j && a[j] > v) {
-				j--;
+	public static int partition1(int[] a,int low, int high) {  
+		int i = low, j = high+1;   //因为pivot选的左侧第一个数，因此从右到左时，最后一个数不能忽略，所以是j=high+1
+		int v = a[low];    //pivot, 切分元素
+		while(true) {
+			//跟上面的partition区别是不用判断i<j,如果判断了[0,2,1]这样的情况就无法正确切分
+			//
+			while(a[++i] < v) {  
+				if(i == high) {
+					break;
+				}
 			}
-			while(i < j && a[i] < v) {
-				i++;
+			while(a[--j] > v) {
+				if(j == low) {
+					break;
+				}
+			}
+			//
+			if(i >= j) {   
+				break;
 			}
 			int temp = a[i];
 			a[i] = a[j];
 			a[j] = temp;
 
 		} 
-		int temp = a[i];
-		a[i] = v;
-		a[lo] = temp;
-		return i;
+		
+		int temp = a[j];
+		a[j] = v;
+		a[low] = temp;
+			
+		return j;
 	}
 	
 	public static void main(String[] args) {
-		int[] a = {6,1,4,9,0,3,3,5,2,7,8};
-		Sort.sort(a);
+//		Scanner sc = new Scanner(in);    
+//        String[] str = sc.nextLine().replace("[", "").replace("]", "").split(",");
+//        int[] data = new int[str.length];
+//        for (int i = 0; i < data.length; i++) {
+//            data[i] = parseInt(str[i]);    
+//        }
+		int[] a = {6,8,1,4,9,0,3,4,5,2,7};
+        
+        Quicksort.sort(a);
+        
+        
 		for(int i = 0;i<a.length;i++) {
 			System.out.println(a[i]);
-		}
+		}       
+//        sc.close();
 	}
-
 }
