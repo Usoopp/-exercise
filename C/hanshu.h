3518c27108def813b41b43471debae5fd4632ea3
monkey <35953989+Usoopp@users.noreply.github.com>
2019-09-25 17:24:45 +0800

    Create hanshu.h

---
+++ C/hanshu.h
@@ -0,0 +1,17 @@
+#ifndef H_FUNCTION_C
+#define H_FUNCTION_C
+#include "stdio.h"
+
+void log();   /*声明查看函数*/
+void regist();   /*声明保存函数*/
+void read_user();   /*声明按姓名查看函数*/
+void view_message();
+void add_message();
+void adminlog1();
+void modify();
+void welcome();  /*声明我的个人信息函数*/
+void liuyan();  //声明留言函数
+void adminlog();
+
+
+#endif
